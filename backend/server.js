483da700321875d6afe4f486d283a478c06294dc
Muhammad Alif Anshar <168310428+alifanshar@users.noreply.github.com>
2025-06-27 19:55:48 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const dashboardRoutes = require('./routes/dashboardRoutes');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
