87fc2e1fc3abed00cfa6b047c368e69457ca4cbd
Muhammad Alif Anshar <168310428+alifanshar@users.noreply.github.com>
2025-06-27 21:52:25 +0800

    Update Register.jsx

--- frontend/src/components/pages/Register/Register.jsx
+++ frontend/src/components/pages/Register/Register.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-const API_URL = 'http://157.10.252.179:3000/api';
+const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 const Register = () => {
     const navigate = useNavigate();
