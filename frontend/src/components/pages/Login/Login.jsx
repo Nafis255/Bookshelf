16e922e87849f2ece39cc2bead7e5edf2a20fc81
Muhammad Alif Anshar <168310428+alifanshar@users.noreply.github.com>
2025-06-27 18:44:17 +0800

    Update Login.jsx

--- frontend/src/components/pages/Login/Login.jsx
+++ frontend/src/components/pages/Login/Login.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import './login.css';
 
 // Ganti dengan URL backend Anda
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'http://localhost:3001/api';
 
 const Login = () => {
     const navigate = useNavigate();
